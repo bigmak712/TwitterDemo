3cb190f3fa712a3e210d6ac51255f08e33728a4c
bigmak712 <bigmak712@gmail.com>
2017-12-18 18:56:49 -0800

    Added user persistence and logout capabilities

--- twitter_alamofire_demo/AppDelegate.swift
+++ twitter_alamofire_demo/AppDelegate.swift
@@ -16,7 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        // MARK: TODO: Check for logged in user
+        // Check for logged in user
+        if User.current != nil {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let navigationVC = storyboard.instantiateViewController(withIdentifier: "TweetsNavigationController")
+            self.window?.rootViewController = navigationVC
+        }
         
         NotificationCenter.default.addObserver(forName: Notification.Name("didLogout"), object: nil, queue: OperationQueue.main) { (Notification) in
             print("Logout notification received")
--- twitter_alamofire_demo/User.swift
+++ twitter_alamofire_demo/User.swift
@@ -10,10 +10,41 @@ import Foundation
 
 class User {
     
+    private static var currentUser: User?
+    
+    // for user persistence
+    var dictionary: [String: Any]?
+    
     var name: String
     
     init(dictionary: [String: Any]) {
+        
+        self.dictionary = dictionary
         name = dictionary["name"] as! String
 
+    }
+    
+    static var current: User? {
+        get {
+            if currentUser == nil {
+                let defaults = UserDefaults.standard
+                if let userData = defaults.data(forKey: "currentUserData") {
+                    let dictionary = try! JSONSerialization.jsonObject(with: userData, options: []) as! [String: Any]
+                    currentUser = User(dictionary: dictionary)
+                }
+            }
+            return currentUser
+        }
+        set (user) {
+            currentUser = user
+            let defaults = UserDefaults.standard
+            if let user = user {
+                let data = try! JSONSerialization.data(withJSONObject: user.dictionary!, options: [])
+                defaults.set(data, forKey: "currentUserData")
+            }
+            else {
+                defaults.removeObject(forKey: "currentUserData")
+            }
+        }
     }
 }
